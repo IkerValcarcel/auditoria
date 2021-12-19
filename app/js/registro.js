474433b538854a9d187142f33ae2339bd09c5d29
Iker Valcarcel <ikervalcarcel@gmail.com>
2021-12-19 21:11:30 +0100

    Medio arreglado xss

--- app/js/registro.js
+++ app/js/registro.js
@@ -12,15 +12,37 @@ function comprobar_datos(){
     
     console.log("Enviando formulario...");
 
-    if(nombre.includes("<") || nombre.includes(">") || nombre.includes("&")|| nombre.includes("$")) {             alert("Posible XSS, evite usar > y <");return false };
-    if(apellidos.includes("<") || apellidos.includes(">") || apellidos.includes("&")|| apellidos.includes("$")) {             alert("Posible XSS, evite usar > y <");return false };
-    if(email.includes("<") || email.includes(">") || email.includes("&")|| email.includes("$")) {             alert("Posible XSS, evite usar > y <");return false };
-    if(telefono.includes("<") || telefono.includes(">") || telefono.includes("&")|| telefono.includes("$")) {             alert("Posible XSS, evite usar > y <");return false };
-    if(mensaje.includes("<") || mensaje.includes(">") || mensaje.includes("&")|| mensaje.includes("$")) {             alert("Posible XSS, evite usar > y <");return false };
-    if(dni.includes("<") || dni.includes(">") || dni.includes("&")|| dni.includes("$")) {             alert("Posible XSS, evite usar > y <");return false };
-    if(nacimiento.includes("<") || nacimiento.includes(">") || nacimiento.includes("&")|| nacimiento.includes("$")) {             alert("Posible XSS, evite usar > y <");return false };
-    if(password.includes("<") || password.includes(">") || password.includes("&")|| password.includes("$")) {             alert("Posible XSS, evite usar > y <");return false };
-password
+    if(nombre.includes("<") || nombre.includes(">") || nombre.includes("&")|| nombre.includes("$")) {
+        alert("Posible XSS, evite usar > y <");
+        return false; 
+    }
+    if(apellidos.includes("<") || apellidos.includes(">") || apellidos.includes("&")|| apellidos.includes("$")) {
+        alert("Posible XSS, evite usar > y <");
+        return false;
+    }
+    if(email.includes("<") || email.includes(">") || email.includes("&")|| email.includes("$")) {
+        alert("Posible XSS, evite usar > y <");
+        return false;
+    }
+    if(telefono.includes("<") || telefono.includes(">") || telefono.includes("&")|| telefono.includes("$")) {
+        alert("Posible XSS, evite usar > y <");
+
+        return false;
+    }
+    if(mensaje.includes("<") || mensaje.includes(">") || mensaje.includes("&")|| mensaje.includes("$")) {
+        alert("Posible XSS, evite usar > y <");return false;}
+    if(dni.includes("<") || dni.includes(">") || dni.includes("&")|| dni.includes("$")) {
+        alert("Posible XSS, evite usar > y <");
+        return false;
+    }
+    if(nacimiento.includes("<") || nacimiento.includes(">") || nacimiento.includes("&")|| nacimiento.includes("$")) {
+        alert("Posible XSS, evite usar > y <");
+        return false;
+    }
+    if(password.includes("<") || password.includes(">") || password.includes("&")|| password.includes("$")) {
+        alert("Posible XSS, evite usar > y <");
+        return false;
+    }
    
     
     if (nombre === null || nombre === ''){
